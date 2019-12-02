a6681a51452d2286ff604c37149df17eb0aaeb5a
ilya-lunyov98 <ilya.lunyov.98@gmail.com>
2019-12-02 23:55:54 +0300

    process new prod

--- app/js/main.js
+++ app/js/main.js
@@ -9,9 +9,17 @@ $(function(){
       dots:true,
       arrows:false,
       slidesToShow: 3,
-      slidesToScroll: 8,
-      
+      slidesToScroll: 8,    
     });
+
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+  });
 
   var mixer = mixitup('.products__inner-box');
 });
